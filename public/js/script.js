06357bcc81eb7b645e7ad5a0af0cacee4e7ba3dd
Nicholas Tonui <kitetnet@gmail.com>
2017-07-22 11:13:14 +0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,14 +2,14 @@
 $(document).ready(function(){
   $(".show_form").click(function(){
     $(".my_form").fadeIn(1500);
-    $("show_form").fadeOut(1500);
+    $(".show_form").fadeOut(1500);
   });
   $(".submit_form").click(function(){
     $(".my_form").fadeOut(1500);
-    $("show_form").fadeIn(1500);
+    $(".show_form").fadeIn(1500);
   });
   $(".cancel_form").click(function(){
     $(".my_form").fadeOut(1500);
-    $("show_form").fadeIn(1500);
+    $(".show_form").fadeIn(1500);
   });
 });
